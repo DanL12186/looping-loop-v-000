5f03122f4b86a2cd321a86cb651cf01ef01e220d
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-02 02:08:17 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def looping
-	#your code here
+	loop do
+    puts "Accio time device! I have found the Timeturner!"
+		return 0 #Yeah, this doesn't exit with control C, just eats up 700% CPU and 12 GB RAM, hands then crashes.
+  end
 end
 
 #call your method here
+looping
